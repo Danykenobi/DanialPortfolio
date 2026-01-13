b9e6d2c5355f5cdd03554622d6e46d559e15cc58
Danykenobi <danialnazim8@gmail.com>
2026-01-14 03:12:48 +0800

    Update resume download link to new Google Drive URL

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1GQ9chYn7cBFLPUw9kKXE6f6gvWMB-9Zn/view?usp=sharing"
+                href="https://drive.google.com/file/d/1jGe_M78BCTr5hGCYNEQHXQiVhkdpWMHo/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download Resume
